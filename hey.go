39e289bbb125111710154d296624b5a774d3ffdc
brendanta <brendan.ta@jacques.com.au>
2021-07-19 12:09:59 +1000

    Add cya

--- hey.go
+++ hey.go
@@ -6,3 +6,8 @@ func Hey(name string) string {
     message := fmt.Sprintf("Hey, %v!", name)
     return message
 }
+
+func Cya(name string) string {
+    message := fmt.Sprintf("Cya %v!", name)
+    return message
+}
